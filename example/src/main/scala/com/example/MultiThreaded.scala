6120aed0cce232bf3c1548c60c2de5552466506a
Paul Horn <knutwalker@gmail.com>
2014-07-19 22:36:13 +0200

    improve multi threaded example

--- example/src/main/scala/com/example/MultiThreaded.scala
+++ example/src/main/scala/com/example/MultiThreaded.scala
@@ -14,15 +14,17 @@ object MultiThreaded extends App {
   class MyThread[A: Writes](client: Client, total: Int, command: A, expected: RespType, f: RespType => Unit) extends Thread {
     private final var _correct = 0
     private final var _incorrect = 0
+    private final val action = (r: RespType) => {
+      if (r == expected) _correct +=1
+      else _incorrect += 1
+      f(r)
+    }
     override def run(): Unit = {
-      for (n <- 1 to total) {
-        val observable: Observable[RespType] = client.command(command)
-        observable.toBlocking.foreach { r =>
-          if (r == expected) _correct +=1
-          else _incorrect += 1
-          f(r)
-        }
+      for (n <- 1 to (total - 1)) {
+        client.command(command).foreach(action)
       }
+      val observable: Observable[RespType] = client.command(command)
+      observable.toBlocking.foreach(action)
     }
 
     def incorrect: Int = _incorrect
@@ -43,18 +45,27 @@ object MultiThreaded extends App {
     cmd"ECHO miau" -> RespBytes("miau")
   )
 
+  val repetitions = args(0).toInt
+
   val threadCount = rrs.size
 
   val threads = rrs map {
-    case (cmd, res) => new MyThread(client, args(0).toInt, cmd, res, r => {
+    case (cmd, res) => new MyThread(client, repetitions, cmd, res, r => {
       if (res != r) println(preview(r) + "  VS.  " + preview(res))
     })
   }
 
+  val start = System.currentTimeMillis()
+
   threads foreach (_.start())
   threads foreach (_.join())
 
-  println("Erroneous threads: ")
+  val end = System.currentTimeMillis()
+
+  private val took = (end - start).toDouble
+  private val requestCount = threadCount * repetitions
+  println(f"finished sending ${requestCount} commands in ${took} ms. That is ${requestCount / took * 1000}%.2f Req/s")
+  println("Erroneous threads (if any): ")
   threads filter (_.incorrect > 0) foreach { t =>
     println(s"Thread: ${t.getName}")
     println(s"  Correct: ${t.correct}")
