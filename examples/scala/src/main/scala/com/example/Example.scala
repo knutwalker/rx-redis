3968dcd15b64ade0b1bf0c41d25376e362c22d31
Paul Horn <knutwalker@gmail.com>
2014-07-23 23:58:04 +0200

    match ws-loc in scala/java example

--- examples/scala/src/main/scala/com/example/Example.scala
+++ examples/scala/src/main/scala/com/example/Example.scala
@@ -11,12 +11,19 @@ object Example extends App {
     toBlocking.foreach(println)
 
   client.get("key")
-  client.mset("foo1" -> "foo1", "foo2" -> "foo2", "foo3" -> "foo3")
+  client.mset(
+    "foo1" -> "foo1",
+    "foo2" -> "foo2",
+    "foo3" -> "foo3")
 
-  val gets = client.get("foo") merge client.get("foo1")
-  val mget = client.mget("foo1", "foo2", "foo4", "foo3", "foo5")
+  val gets =
+    client.get("foo") merge client.get("foo1")
 
-  gets.merge(mget).foreach(r => println("GET or MGET = " + r))
+  val mget =
+    client.mget("foo1", "foo2", "foo4", "foo3", "foo5")
+
+  gets.merge(mget).
+    foreach(r => println("GET or MGET = " + r))
 
   val mixed = client.ping() ++
     client.echo("42") ++
