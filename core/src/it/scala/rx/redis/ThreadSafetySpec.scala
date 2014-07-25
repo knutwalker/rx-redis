dde3d777f8f6e2c44db369883e22741fe8120e67
Paul Horn <knutwalker@gmail.com>
2014-07-26 01:08:10 +0200

    multi threaded examples as integration test

---
+++ core/src/it/scala/rx/redis/ThreadSafetySpec.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 Paul Horn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rx.redis
+
+import org.scalatest.FunSuite
+import rx.functions.Action1
+import rx.redis.client.RawClient
+import rx.redis.resp.{DataType, RespBytes, RespType}
+
+
+class ThreadSafetySpec extends FunSuite {
+
+  val total = 4000
+  val threadCount = 25
+
+  private class TestThread(
+      commandToSend: DataType,
+      expected: RespType,
+      getClient: => RawClient,
+      shutdown: RawClient => Unit)
+    extends Thread {
+
+    private final var _correct = 0
+    private final var _incorrect = 0
+    private final val action: Action1[_ >: RespType] =
+      new Action1[RespType] {
+        def call(t1: RespType): Unit = {
+          if (t1 == expected) _correct +=1
+          else _incorrect += 1
+        }
+      }
+
+    private final val client = getClient
+
+    override def run(): Unit = {
+      for (n <- 1 until total) {
+        client.command(commandToSend).forEach(action)
+      }
+      val last = client.command(commandToSend)
+      last.toBlocking.forEach(action)
+
+      shutdown(client)
+    }
+
+    def incorrect: Int = _incorrect
+    def correct: Int = _correct
+  }
+
+  test("should not diverge when marked as shareable and shared amongst threads") {
+
+    val client = RawClient("127.0.0.1", 6379, shareable = true)
+
+    def createThread(n: Int) = new TestThread(cmd"ECHO ${n.toString}", RespBytes(n.toString), client, _ => ())
+
+    val threads = List.tabulate(threadCount)(createThread)
+
+    threads foreach (_.start())
+    threads foreach (_.join())
+
+    threads foreach { t =>
+      assert(t.correct == total)
+      assert(t.incorrect == 0)
+    }
+  }
+
+  test("should not diverge when not shareable, but used in isolation") {
+
+    def client = RawClient("127.0.0.1", 6379, shareable = false)
+
+    def createThread(n: Int) = new TestThread(cmd"ECHO ${n.toString}", RespBytes(n.toString), client, { c =>
+      c.shutdown()
+      c.closedObservable.toBlocking.lastOrDefault(())
+    })
+
+    val threads = List.tabulate(threadCount)(createThread)
+
+    threads foreach (_.start())
+    threads foreach (_.join())
+
+    threads foreach { t =>
+      assert(t.correct == total)
+      assert(t.incorrect == 0)
+    }
+  }
+
+  test("should diverge when not marked as shareable but shared amongst threads") {
+
+    val client = RawClient("127.0.0.1", 6379, shareable = false)
+
+    def createThread(n: Int) = new TestThread(cmd"ECHO ${n.toString}", RespBytes(n.toString), client, _ => ())
+
+    val threads = List.tabulate(threadCount)(createThread)
+
+    threads foreach (_.start())
+    threads foreach (_.join())
+
+    threads foreach { t =>
+      assert(t.correct < total)
+      assert(t.incorrect > 0)
+    }
+  }
+}
--- examples/scala/src/main/scala/com/example/package.scala
+++ examples/scala/src/main/scala/com/example/package.scala
@@ -15,44 +15,9 @@
  */
 package com
 
-import rx.redis._
-import rx.redis.resp.{DataType, RespBytes, RespString, RespType}
+import rx.redis.resp.RespType
 
 package object example {
 
   val preview = (r: RespType) => r.toString.replaceAllLiterally("\r\n", "\\r\\n").take(50)
-
-  val rrs: List[(DataType, DataType)] = List(
-    cmd"PING" -> RespString("PONG"),
-    cmd"ECHO foo" -> RespBytes("foo"),
-    cmd"ECHO bar" -> RespBytes("bar"),
-    cmd"ECHO baz" -> RespBytes("baz"),
-    cmd"ECHO qux" -> RespBytes("qux"),
-    cmd"ECHO foobar" -> RespBytes("foobar"),
-    cmd"ECHO barbaz" -> RespBytes("barbaz"),
-    cmd"ECHO quxall" -> RespBytes("quxall"),
-    cmd"ECHO miau" -> RespBytes("miau")
-  )
-
-  val threadCount = rrs.size
-
-  def doMultiThreaded(repetitions: Int, threads: List[RedisThread]) = {
-
-    val start = System.currentTimeMillis()
-
-    threads foreach (_.start())
-    threads foreach (_.join())
-
-    val end = System.currentTimeMillis()
-
-    val took = (end - start).toDouble
-    val requestCount = threadCount * repetitions
-    println(f"finished sending ${requestCount} commands in ${took} ms. That is ${requestCount / took * 1000}%.2f Req/s")
-    println("Erroneous threads (if any): ")
-    threads filter (_.incorrect > 0) foreach { t =>
-      println(s"Thread: ${t.getName}")
-      println(s"  Correct: ${t.correct}")
-      println(s"  Incorrect: ${t.incorrect}")
-    }
-  }
 }
