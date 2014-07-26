eec8f82af44320250b3b10dd3056debce7153b8b
Paul Horn <knutwalker@gmail.com>
2014-07-26 17:08:39 +0200

    license formatting updates

--- language-bindings/scala/src/main/scala/rx/redis/api/Client.scala
+++ language-bindings/scala/src/main/scala/rx/redis/api/Client.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package rx.redis.api
 
 import scala.concurrent.duration.{ Deadline, FiniteDuration }
