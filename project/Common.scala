8c8571243dc330655e17291408a8f06e7c891ca3
Paul Horn <knutwalker@gmail.com>
2014-07-26 12:45:18 +0200

    run integration tests when releasing

--- project/Common.scala
+++ project/Common.scala
@@ -1,9 +1,9 @@
-import sbt._
-import sbtrelease._
-import sbtrelease.ReleasePlugin._
-import sbtrelease.ReleasePlugin.ReleaseKeys._
 import com.typesafe.sbt.SbtPgp.PgpKeys._
+import sbt._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleaseStateTransformations._
+import sbtrelease._
 
 
 object Common {
@@ -14,12 +14,28 @@ object Common {
     extracted.runAggregated(publishSigned in Global in ref, st)
   }
 
+  lazy val runIntegrationTest = ReleaseStep(action = { st: State =>
+    if (!st.get(skipTests).getOrElse(false)) {
+      val extracted = Project.extract(st)
+      val ref = extracted.get(Keys.thisProjectRef)
+      extracted.runAggregated(Keys.test in IntegrationTest in ref, st)
+    } else st
+  })
+
   lazy val signedReleaseSettings = releaseSettings ++ Seq(
-    releaseProcess ~= { s: Seq[ReleaseStep] =>
-      s map {
-        case `publishArtifacts` => publishArtifacts.copy(action = publishSignedAction)
-        case s => s
-      } map (_.copy(enableCrossBuild = false))
-    }
+    releaseProcess := List[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runClean,
+      runTest,
+      runIntegrationTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      publishArtifacts.copy(action = publishSignedAction),
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    ).map(_.copy(enableCrossBuild = false))
   )
 }
