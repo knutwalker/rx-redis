f5f3a4252548e16ca3f4035c5afb78fd934fc049
Paul Horn <knutwalker@gmail.com>
2015-04-19 16:29:56 +0200

    Add custom format example for Scala as it exists for Java

--- examples/java/src/main/java/com/example/CustomFormatExample.java
+++ examples/java/src/main/java/com/example/CustomFormatExample.java
@@ -27,11 +27,15 @@ public final class CustomFormatExample {
 
     final Client client = RxRedis.connect("localhost", 6379);
 
-    final Person knut = new Person("Knütsen \uD83D\uDCA9", 27);
+    final Person knut = new Person("Knütß \uD83D\uDCA9", 27);
     client.setAs("person:knut", knut, Person.BYTES_FORMAT);
 
-    client.get("person:knut").forEach(s -> System.out.println("Person as string: " + s.orElse("")));
-    client.getBytes("person:knut").forEach(b -> System.out.println("Person as bytes: " + b.map(Arrays::toString).orElse("")));
+    client.get("person:knut")
+        .forEach(s -> System.out.println("Person as string: " + s.orElse("")));
+
+    client.getBytes("person:knut")
+        .forEach(b -> System.out.println("Person as bytes: " + b.map(Arrays::toString).orElse("")));
+
     client.getAs("person:knut", Person.BYTES_FORMAT)
         .forEach(p -> System.out.println("Person as Person: " + p.orElse(null)));
 
